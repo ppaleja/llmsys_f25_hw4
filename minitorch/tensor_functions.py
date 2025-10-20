ebacf737046337e1cc8c8f9f3c79b466cf49da8b
ppaleja <46587956+ppaleja@users.noreply.github.com>
2025-10-20 15:03:29 -0700

    bind attention softmax backward pass and update forward method to save input tensor

--- minitorch/tensor_functions.py
+++ minitorch/tensor_functions.py
@@ -383,14 +383,15 @@ class Attn_Softmax(Function):
     @staticmethod
     def forward(ctx: Context, inp: Tensor, mask: Tensor) -> Tensor:
       #   BEGIN ASSIGN4_1_1
-      ctx.save_for_backward(mask)
+      ctx.save_for_backward(inp, mask)
       return inp.f.attn_softmax_fw(inp, mask)
       #   END ASSIGN4_1_1
 
     @staticmethod
     def backward(ctx: Context, out_grad: Tensor) -> Tensor:
       #   BEGIN ASSIGN4_1_2
-      raise NotImplementedError("Need to implement for Assignment 3")
+      (inp, mask) = ctx.saved_values
+      return out_grad.f.attn_softmax_bw(out_grad, inp, mask)
       #   END ASSIGN4_1_2
 
 
